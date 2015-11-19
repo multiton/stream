0e58c8fb12f37f5e9ffee0d276da21d83eff4508
multiton <k_smithus@yahoo.com>
2015-11-18 23:22:07 -0500

    Category Entity created

---
+++ Stream/Stream.Domain.Entity/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stream.Domain.Entity
+{
+    public class Category
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+        
+        public Guid? ParentId { get; set; }
+
+        public virtual Category Parent { get; set; }
+        
+        public virtual ICollection<Category> Categories { get; set; } 
+    }
+}
