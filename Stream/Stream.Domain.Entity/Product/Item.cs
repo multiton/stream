74ba6709d5e8b6fdad3a4d80f4b3a758ee716801
multiton <k_smithus@yahoo.com>
2015-11-20 11:46:24 -0500

    Item entity added

---
+++ Stream/Stream.Domain.Entity/Product/Item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Stream.Domain.Entity.Product
+{
+    public class Item
+    {
+        public Guid Id { get; set; } // = Guid.NewGuid();
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
