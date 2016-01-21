b46638a1af39a04504d876a1f8f5812e3e30fd63
Sergey V. Kovalenko <k_smithus@yahoo.com>
2016-01-20 23:23:16 -0500

    Massive refactoring based on mentoring sessions with Nicole

---
+++ Stream/Stream.IoC/IoCHost.cs
@@ -0,0 +1,54 @@
+﻿using Castle.Windsor;
+
+using Stream.IoC.Facade;
+using Stream.IoC.Installer;
+
+namespace Stream.IoC
+{
+    public sealed class IoCHost : IObjectResolvable
+    {
+        private static readonly object SyncRoot = new object();
+
+        private readonly IWindsorContainer container;
+
+        private static volatile IoCHost instance;
+
+        public static IObjectResolvable Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (SyncRoot)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new IoCHost();
+                        }
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        private IoCHost()
+        {
+            this.container = new WindsorContainer();
+            this.Register();
+        }
+
+        private void Register()
+        {
+            this.container.Install(
+                new DataContextInstaller(),
+                new RepositoryInstaller(),
+                new ServiceInstaller());
+        }
+
+        public T Get<T>()
+        {
+            return this.container.Resolve<T>();
+        }
+    }
+}
