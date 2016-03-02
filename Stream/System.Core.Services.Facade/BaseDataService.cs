a5788a453e5d6cb71b11d76ee9fd0d9eae8af9fd
Sergey V. Kovalenko <k_smithus@yahoo.com>
2016-03-02 14:49:09 -0500

    Code cleanup

---
+++ Stream/System.Core.Services.Facade/BaseDataService.cs
@@ -0,0 +1,19 @@
+﻿using Stream.DAL.Facade;
+
+namespace System.Core.Services.Facade
+{
+    public abstract class BaseDataService : IDisposable
+    {
+        protected readonly IUnitOfWork UnitOfWork;
+
+        protected BaseDataService(IUnitOfWork unitOfWork)
+        {
+            this.UnitOfWork = unitOfWork;
+        }
+
+        public void Dispose()
+        {
+            this.UnitOfWork.Dispose();
+        }
+    }
+}
