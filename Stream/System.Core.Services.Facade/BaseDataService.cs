d06d932eb5e19de111185c08b861aba09de0aef7
Kovalenko <skovalen@teksystems.com>
2016-03-02 14:41:58 -0500

    Add pending refference to repository to BaseDataService

---
+++ Stream/System.Core.Services.Facade/BaseDataService.cs
@@ -0,0 +1,22 @@
+﻿using Stream.DAL.Facade;
+
+namespace System.Core.Services.Facade
+{
+    public abstract class BaseDataService : IDisposable
+    {
+        // ToDo: Pending
+        // private readonly EntityFrameworkRepository<TId, TEntity, TIdInitializer> Repository;
+
+        protected readonly IUnitOfWork UnitOfWork;
+
+        protected BaseDataService(IUnitOfWork unitOfWork)
+        {
+            this.UnitOfWork = unitOfWork;
+        }
+
+        public void Dispose()
+        {
+            this.UnitOfWork.Dispose();
+        }
+    }
+}
